31211b3aefdfabae3319ee28445225666da82dbd
TanyRM <tanielemadu@outlook.com>
2025-05-23 12:18:57 -0300

    Cenário da fase raiva

--- scripts/personagem/Orb.gd
+++ scripts/personagem/Orb.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 @export var orb_type: String = "vazio"
 var is_collected = false
+@onready var collect_sound = $CollectSound
 
 func _ready():
 	$AnimatedSprite2D.play(orb_type)
@@ -10,12 +11,13 @@ func _ready():
 
 func _on_body_entered(body: Node) -> void:
 	if body.name == "Cynthia" and not is_collected:
-		coletar_orb(body)
-
-func coletar_orb(player):
-	is_collected = true
-	$CollisionShape2D.set_deferred("disabled", true)
-	player.add_following_orb(self)
-	if not Globals.save_data["orbs_collected"].has(orb_type):
-		Globals.save_data["orbs_collected"].append(orb_type)
-	Globals.save_game()
+		is_collected = true
+		$CollisionShape2D.set_deferred("disabled", true)
+		body.add_following_orb(self)
+		if not Globals.save_data["orbs_collected"].has(orb_type):
+			Globals.save_data["orbs_collected"].append(orb_type)
+		Globals.save_game()
+		
+		# toca o som
+		if collect_sound:
+			collect_sound.play()
